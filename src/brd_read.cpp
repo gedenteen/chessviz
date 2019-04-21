ec43eb0ac28cc38c95226fb95a5ecf6037262d3b
gedenteen <razumow.dmitrij@yandex.ru>
2019-04-21 12:13:07 +0700

    Add empty brd_read.cpp and brd_read.h

---
+++ src/brd_read.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int brd_read()
+{
+    ifstream inpt("input.txt");
+    //int i;
+    string sg;
+    //for (inpt >> sg; !inpt.eof(); inpt >> sg) {
+    //    cout << sg << endl;
+    //}
+    return 0;
+}
---
+++ src/brd_read.h
@@ -0,0 +1,6 @@
+#ifndef BRD_READ_H
+#define BRD_READ_H
+
+int brd_read();
+
+#endif
